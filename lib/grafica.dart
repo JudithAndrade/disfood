7a4f914d105820e5a30c021bc342d712ece6e8d1
Judith AdileneAndrade Perez <al19760544@ite.edu.mx>
2024-10-20 15:26:50 -0700

    disfood

--- lib/grafica.dart
+++ lib/grafica.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:pie_chart/pie_chart.dart'; // Asegúrate de tener el paquete pie_chart en tu pubspec.yaml
+import 'package:syncfusion_flutter_charts/charts.dart'; // Paquete de Syncfusion para gráficos
 
 class GraficaScreen extends StatefulWidget {
   const GraficaScreen({super.key});
@@ -33,7 +33,7 @@ class _GraficaScreenState extends State<GraficaScreen> {
     } catch (e) {
       print('Error obteniendo los datos: $e');
       setState(() {
-        isLoading = false; // Asegúrate de detener la carga en caso de error
+        isLoading = false;
       });
     }
   }
@@ -42,39 +42,17 @@ class _GraficaScreenState extends State<GraficaScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Gráfica de Anillo'),
+        title: const Text('Gráficas Deslizables'),
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : data != null
-              ? Center(
-                  child: PieChart(
-                    dataMap: _generateDataMap(data!), // Llamamos a la función para generar el mapa de datos
-                    animationDuration: const Duration(milliseconds: 800),
-                    chartLegendSpacing: 32,
-                    chartRadius: MediaQuery.of(context).size.width / 2.5, // Ajusta el tamaño
-                    colorList: _generateColorList(), // Llamamos a la función para generar la lista de colores
-                    initialAngleInDegree: 0,
-                    chartType: ChartType.ring,
-                    ringStrokeWidth: 32,
-                    centerText: "Días",
-                    legendOptions: LegendOptions(
-                      showLegendsInRow: false,
-                      legendPosition: LegendPosition.right,
-                      showLegends: true,
-                      legendShape: BoxShape.circle,
-                      legendTextStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    chartValuesOptions: ChartValuesOptions(
-                      showChartValueBackground: true,
-                      showChartValues: true,
-                      showChartValuesInPercentage: false,
-                      showChartValuesOutside: false,
-                      decimalPlaces: 1,
-                    ),
-                  ),
+              ? PageView(
+                  children: [
+                    _buildBarChart(), // Gráfico de barras: Cantidad por Día
+                    _buildLineChart(), // Gráfico de líneas: Cantidad por Día y Hora
+                    _buildRadialBarChart(), // Gráfico radial: Cantidad por Día
+                  ],
                 )
               : const Center(
                   child: Text('No hay datos disponibles'),
@@ -82,30 +60,132 @@ class _GraficaScreenState extends State<GraficaScreen> {
     );
   }
 
-  void _showSnackbar(BuildContext context, String key, String value) {
-    final snackBar = SnackBar(
-      content: Text('Día: $key, Valor: $value'),
-      duration: const Duration(seconds: 2),
+  // Gráfico de barras mostrando la cantidad de alimentos por día
+  Widget _buildBarChart() {
+    List<BarChartData> barData = [
+      BarChartData('Lunes', 200, const Color.fromARGB(255, 199, 118, 219)),
+      BarChartData('Martes', 150, const Color.fromARGB(255, 0, 140, 255)),
+      BarChartData('Miércoles', 100, const Color.fromARGB(255, 248, 59, 255)),
+      BarChartData('Jueves', 250, const Color.fromARGB(255, 76, 119, 175)),
+      BarChartData('Viernes', 300, Colors.blue),
+      BarChartData('Sábado', 400, Colors.indigo),
+      BarChartData('Domingo', 350, Colors.purple),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Container(
+        height: 300, // Ajusta la altura aquí para hacerla más pequeña
+        child: SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          title: ChartTitle(text: 'Cantidad de Alimentos por Día'),
+          legend: Legend(isVisible: false),
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: <CartesianSeries>[
+            ColumnSeries<BarChartData, String>(
+              dataSource: barData,
+              xValueMapper: (BarChartData data, _) => data.day,
+              yValueMapper: (BarChartData data, _) => data.amount,
+              name: 'Cantidad',
+              dataLabelSettings: DataLabelSettings(isVisible: true),
+              pointColorMapper: (BarChartData data, _) => data.color, // Asigna el color desde los datos
+            ),
+          ],
+        ),
+      ),
     );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-  Map<String, double> _generateDataMap(Map<String, dynamic> data) {
-    Map<String, double> dataMap = {};
-    data.forEach((key, value) {
-      double numericValue = double.tryParse(value.toString()) ?? 0; // Convertir a numérico
-      dataMap[key] = numericValue; // Añadir al mapa de datos
-    });
-    return dataMap;
+  // Gráfico de líneas mostrando la cantidad de alimentos por día y hora
+  Widget _buildLineChart() {
+    List<BarChartData> hourlyData = [
+      BarChartData('Lunes\n08:00', 200, Colors.purpleAccent),
+      BarChartData('Lunes\n12:00', 150, Colors.purpleAccent),
+      BarChartData('Martes\n08:00', 180, Colors.purpleAccent),
+      BarChartData('Martes\n12:00', 120, Colors.purpleAccent),
+      BarChartData('Miércoles\n08:00', 160, Colors.purpleAccent),
+      BarChartData('Miércoles\n12:00', 140, Colors.purpleAccent),
+      BarChartData('Jueves\n08:00', 220, Colors.purpleAccent),
+      BarChartData('Jueves\n12:00', 130, Colors.purpleAccent),
+      BarChartData('Viernes\n08:00', 250, Colors.purpleAccent),
+      BarChartData('Viernes\n12:00', 270, Colors.purpleAccent),
+      BarChartData('Sábado\n08:00', 300, Colors.purpleAccent),
+      BarChartData('Sábado\n12:00', 320, Colors.purpleAccent),
+      BarChartData('Domingo\n08:00', 280, Colors.purpleAccent),
+      BarChartData('Domingo\n12:00', 290, const Color.fromARGB(255, 64, 114, 251)),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SfCartesianChart(
+        primaryXAxis: CategoryAxis(),
+        title: ChartTitle(text: 'Cantidad de Alimentos por Día y Hora'),
+        legend: Legend(isVisible: true),
+        tooltipBehavior: TooltipBehavior(enable: true),
+        series: <CartesianSeries>[
+          LineSeries<BarChartData, String>(
+            dataSource: hourlyData,
+            xValueMapper: (BarChartData data, _) => data.day,
+            yValueMapper: (BarChartData data, _) => data.amount,
+            name: 'Cantidad',
+            dataLabelSettings: DataLabelSettings(isVisible: true),
+            color: Colors.purpleAccent, // Color del gráfico
+          ),
+        ],
+      ),
+    );
   }
 
-  List<Color> _generateColorList() {
+  // Gráfico radial mostrando la cantidad de alimentos por día
+  Widget _buildRadialBarChart() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SfCircularChart(
+        title: ChartTitle(text: 'Gráfico Radial de Alimentos por Día'),
+        legend: Legend(isVisible: true),
+        series: <CircularSeries>[
+          RadialBarSeries<BarChartData, String>(
+            dataSource: _createBarChartData(),
+            xValueMapper: (BarChartData data, _) => data.day,
+            yValueMapper: (BarChartData data, _) => data.amount,
+            dataLabelSettings: DataLabelSettings(isVisible: true),
+            maximumValue: 400, // Ajusta según tus datos
+            radius: '80%',
+            pointColorMapper: (BarChartData data, _) {
+              // Mapeo de colores para cada porción del gráfico radial
+              return data.day == 'Lunes' ? const Color.fromARGB(255, 60, 153, 230) :
+                     data.day == 'Martes' ? Colors.purple :
+                     data.day == 'Miércoles' ? Colors.blueAccent :
+                     data.day == 'Jueves' ? Colors.purpleAccent :
+                     data.day == 'Viernes' ? Colors.lightBlue :
+                     data.day == 'Sábado' ? Colors.indigo :
+                     Colors.deepPurple; // Color para Domingo
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Genera datos para el gráfico radial
+  List<BarChartData> _createBarChartData() {
     return [
-      Colors.redAccent,
-      Colors.blueAccent,
-      Colors.greenAccent,
-      Colors.orangeAccent,
-      Colors.purpleAccent,
+      BarChartData('Lunes', 200, const Color.fromARGB(255, 54, 105, 244)),
+      BarChartData('Martes', 150, const Color.fromARGB(255, 222, 119, 243)),
+      BarChartData('Miércoles', 100, const Color.fromARGB(255, 34, 223, 236)),
+      BarChartData('Jueves', 250, const Color.fromARGB(255, 158, 76, 175)),
+      BarChartData('Viernes', 300, Colors.blue),
+      BarChartData('Sábado', 400, Colors.indigo),
+      BarChartData('Domingo', 350, Colors.purple),
     ];
   }
+}
+
+// Clase para representar los datos del gráfico de barras
+class BarChartData {
+  final String day;
+  final double amount;
+  final Color color; // Nuevo campo para el color
+
+  BarChartData(this.day, this.amount, this.color);
 }
